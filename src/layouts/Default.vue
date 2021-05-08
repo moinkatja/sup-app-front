780af9da18bed97ccafc250e1f6f4857995bb834
Katja Blau <blau@poprocket.com>
2021-05-08 15:29:45 +0200

    initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title> goSUP </v-toolbar-title>
+      <v-text-field
+        placeholder = "Search"
+        class = "ml-8"
+        style="max-width: 350px"
+        prepend-inner-icon = "mdi-magnify"
+        outlined
+        rounded
+        dense
+        hide-details
+      />
+      <v-spacer/>
+    </v-app-bar>
+    <v-main>
+      <slot/>
+    </v-main>
+ 
+  </v-app>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName,
+    siteDescription
+  }
+}
+</static-query>
+
+ 
